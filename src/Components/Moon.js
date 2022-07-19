3fafe8e5fc8947de39f963ab9195e2247c83df8e
Sarina Chang <sarinachang7@gmail.com>
2022-07-19 03:44:23 -0400

    sun layout

--- src/Components/Moon.js
+++ src/Components/Moon.js
@@ -2,29 +2,27 @@ import React from 'react';
 import '../css/Moon.css';
 
 function Moon({ chosenDateInfo }) {
-  // change moon shape depending on phase, check API
   const astro = chosenDateInfo.astro;
-  console.log(chosenDateInfo);
+
+  const moonInfo = [
+    { id: 0, name: 'Rise', info: astro.moonrise },
+    { id: 1, name: 'Set', info: astro.moonset },
+    { id: 2, name: 'Illumination', info: `${astro.moon_illumination}%` },
+  ];
   return (
     <>
       <div className='moon-card'>
         <h3>{astro.moon_phase}</h3>
         <div className='moon-img'>
-          <div className='disc'></div>
+          <div className='moon-disc'></div>
         </div>
-        <div className='moon-info'>
-          <div className='moonrise'>
-            <h3>Rise</h3>
-            <h3>{astro.moonrise}</h3>
-          </div>
-          <div className='moonset'>
-            <h3>Set</h3>
-            <h3>{astro.moonset}</h3>
-          </div>
-          <div className='illum'>
-            <h3>Illumination</h3>
-            <h3>{astro.moon_illumination}%</h3>
-          </div>
+        <div className='moon-info-container'>
+          {moonInfo?.map((moon) => (
+            <div className='moon-info' key={moon.id}>
+              <h3>{moon.name}</h3>
+              <h3>{moon.info}</h3>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/Components/Sun.js
+++ src/Components/Sun.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import '../css/Sun.css';
 
 function Sun({ chosenDateInfo }) {
   const astro = chosenDateInfo.astro;
   const day = chosenDateInfo.day;
 
+  const sunInfo = [
+    { id: 0, name: 'Rise', info: astro.sunrise },
+    { id: 1, name: 'Set', info: astro.sunset },
+    { id: 2, name: 'Humidity', info: `${day.avghumidity}%` },
+  ];
   return (
-    <div className='sun-card'>
-      <h1>SUN</h1>
-      <p>Rise: {astro.sunrise}</p>
-      <p>Set: {astro.sunset}</p>
-      <p>Humidity: {day.avghumidity}</p>
-    </div>
+    <>
+      <div className='sun-card'>
+        <h3>SUN</h3>
+        <div className='sun-img'>
+          <div className='sun-disc'></div>
+        </div>
+        <div className='sun-info-container'>
+          {sunInfo?.map((sun) => (
+            <div className='sun-info' key={sun.id}>
+              <h3>{sun.name}</h3>
+              <h3>{sun.info}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
