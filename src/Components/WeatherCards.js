bea41978d3f0be032e5c948223c2b6826dc0e39e
Sarina Chang <sarinachang7@gmail.com>
2022-07-20 05:11:51 -0400

    import css file error

--- src/Components/WeatherCards.js
+++ src/Components/WeatherCards.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Current from './Current';
 import { temperatureIcon, sunIcon, moonIcon, myInfo } from './Icons';
 import Info from './Info';
+import '../css/index.css';
+import '../css/root.css';
 import '../css/Stars.css';
 
 function WeatherCards({ weatherData }) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/Index.css';
+import './css/index.css';
 import WeatherApp from './Components/WeatherApp';
 import reportWebVitals from './reportWebVitals';
 
